6c174019c033b257ea115dfa92b62732d091fbaa
khemit86 <khemit86@gmail.com>
2024-03-12 18:40:36 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(5000,()=>{
-
-    console.log("hii");
-
-
+app.get("/",(req,res)=>{
+    res.send('Hello World')   
 })
+
+app.listen(5000)
