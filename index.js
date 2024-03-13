4d340dada55fcb647d54140e27b5ea0c23538a72
khemit86 <khemit86@gmail.com>
2024-03-13 13:17:39 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const port  = 5000;
 
 app.get("/",(req,res)=>{
+    console.log("Received request for /");
     res.send('Hello World')   
 })
 
-app.listen(5000)
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
 
 module.exports = app;
