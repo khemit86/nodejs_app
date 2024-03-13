ac8ac29bda1925bb72f0d4d07996fe573ef84204
khemit86 <khemit86@gmail.com>
2024-03-13 14:14:29 +0530

    update

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get("/",(req,res)=>{
     res.send('Hello World')   
 })
 
-app.listen(port, () => {
+/*app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-});
+});*/
 
 module.exports = app;
